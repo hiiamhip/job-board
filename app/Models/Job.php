5216659dc977d87c3ab8ec6fcb7a8c39c77731ad
hiiamhip <trantuanhiep420@gmail.com>
2025-06-25 13:42:00 +0000

    file uploads

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,19 +2,35 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Query\Builder as QueryBuilder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Job extends Model
 {
     use HasFactory;
 
-     public function employer(): BelongsTo
+    public function employer(): BelongsTo
     {
         return $this->belongsTo(Employer::class);
+    }
+
+    public function jobApplications(): HasMany
+    {
+        return $this->hasMany(JobApplication::class);
+    }
+
+    public function hasUserApplied(Authenticatable|User|int $user): bool
+    {
+        return $this->where('id', $this->id)
+            ->whereHas(
+                'jobApplications',
+                fn($query) => $query->where('user_id', '=', $user->id ?? $user)
+            )->exists();
     }
 
     public static array $experience = ['entry', 'intermediate', 'senior'];
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\JobApplicationController;
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\MyJobApplicationController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,3 +26,10 @@ Route::resource('auth', AuthController::class)
 Route::delete('logout', fn() => to_route('auth.destroy'))->name('logout');
 Route::delete('auth', [AuthController::class, 'destroy'])
     ->name('auth.destroy');
+
+Route::middleware('auth')->group(function () {
+    Route::resource('job.application', JobApplicationController::class)
+    ->only(['create', 'store']);
+    Route::resource('my-job-applications', MyJobApplicationController::class)
+        ->only(['index', 'destroy']);
+});
