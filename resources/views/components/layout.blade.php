0311d49a5a7e6be85d488f830c46c7d276a75513
hiiamhip <trantuanhiep420@gmail.com>
2025-06-25 17:51:28 +0000

    employer

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -27,6 +27,9 @@
                     </a>
                 </li>
                 <li>
+                    <a href="{{ route('my-jobs.index') }}">My Jobs</a>
+                </li>
+                <li>
                     <form action="{{ route('auth.destroy') }}" method="POST">
                         @csrf
                         @method('DELETE')
@@ -52,7 +55,7 @@
     @if (session('error'))
         <div role="alert" class="my-8 rounded-md border-l-4 border-red-300 bg-red-100 p-4 text-red-700 opacity-75">
             <p class="font-bold">Error!</p>
-            <p>{{ session('error') }}</p>Add commentMore actions
+            <p>{{ session('error') }}</p>
         </div>
     @endif
 
