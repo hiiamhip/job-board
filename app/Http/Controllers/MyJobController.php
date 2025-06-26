50187ff90b0fb3854cd4a2b4c04474b2fe42515a
hiiamhip <trantuanhiep420@gmail.com>
2025-06-26 10:45:59 +0000

    soft delete

--- app/Http/Controllers/MyJobController.php
+++ app/Http/Controllers/MyJobController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\JobRequest;
+use App\Models\Job;
 use Illuminate\Http\Request;
 
 class MyJobController extends Controller
@@ -11,7 +13,16 @@ class MyJobController extends Controller
      */
     public function index()
     {
-        return view('my_job.index');
+        $this->authorize('viewAnyEmployer', Job::class);
+        return view(
+            'my_job.index',
+            [
+                'jobs' => auth()->user()->employer
+                    ->jobs()
+                    ->with(['employer', 'jobApplications', 'jobApplications.user'])
+                    ->get()
+            ]
+        );
     }
 
     /**
@@ -19,46 +30,39 @@ class MyJobController extends Controller
      */
     public function create()
     {
-        //
+        $this->authorize('create', Job::class);
+        return view('my_job.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function store(JobRequest $request)
     {
-        //
+        $this->authorize('create', Job::class);
+
+        auth()->user()->employer->jobs()->create($request->validated());
+
+        return redirect()->route('my-jobs.index')
+            ->with('success', 'Job created successfully.');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function edit(Job $myJob)
     {
-        //
+        $this->authorize('update', $myJob);
+        return view('my_job.edit', [
+            'job' => $myJob
+        ]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
+    public function update(JobRequest $request, Job $myJob)
     {
-        //
+        $this->authorize('update', $myJob);
+        $myJob->update($request->validated());
+        return redirect()->route('my-jobs.index')->with('success', 'Job updated successfully.');
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function destroy(Job $myJob)
     {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        $myJob->delete();
+        return redirect()->route('my-jobs.index')
+            ->with('success', 'Job deleted.');
     }
 }
--- app/Models/Job.php
+++ app/Models/Job.php
@@ -9,10 +9,20 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Job extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'location',
+        'salary',
+        'description',
+        'experience',
+        'category'
+    ];
 
     public function employer(): BelongsTo
     {
